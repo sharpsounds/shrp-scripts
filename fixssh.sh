5df6a61449dc7350d58f4956dc0413206a909e7e
Wesley Oliver <50159898+sharpsounds@users.noreply.github.com>
2024-09-18 22:07:18 +1000

    fixed colors

--- fixssh.sh
+++ fixssh.sh
@@ -1,30 +1,43 @@
 #!/bin/bash
-red='\e[1;31m%s\e[0m\n'                                                                                                                                                      
-green='\e[1;32m%s\e[0m\n'
-yellow='\e[1;33m%s\e[0m\n'
+noformat='\e[0m'
+red='\e[31m'
+green='\e[32m'
+yellow='\e[33m'
+blue='\e[34m'
+magenta='\e[35m'
+cyan='\e[36m'
+grey='\e[90m'
+b_red='\e[91m'
+b_green='\e[92m'
+b_yellow='\e[93m'
+b_blue='\e[94m'
+b_magenta='\e[95m'
+b_cyan='\e[96m'
+
+
 clear
 
-printf "$green" "\n INFO: Checking if script is runnning as ROOT \n"
+printf "\n $b_yellow INFO: $_yellow Checking if script is runnning as ROOT \n"
 if [ `id -u` != '0' ]; then
-    printf $red "Root privileges are required for this script."
+    printf $b_red "Root privileges are required for this script."
 else
-    printf $green "You are root."
+    printf $b_green "You are root."
 fi
 
 cd /
-printf $green "\n INFO: Appending PasswordAuthentication yes to /etc/ssh/ssh_config! \n"
+printf "\n $b_green INFO: $green Appending PasswordAuthentication yes to /etc/ssh/ssh_config! \n"
 sleep 2
 echo "PasswordAuthentication yes" >> /etc/ssh/ssh_config
 
-printf $green "\n INFO: Appending PasswordAuthentication yes to /etc/ssh/sshd_config! \n"
+printf "\n $b_green INFO: $green Appending PasswordAuthentication yes to /etc/ssh/sshd_config! \n"
 sleep 2
 echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
 
-printf $green "\n INFO: Replacing /etc/ssh/sshd_config.d/60-cloudimg-settings.conf with one that contains PasswordAuthentication yes! \n"
+printf "\n $b_green INFO: $green Replacing /etc/ssh/sshd_config.d/60-cloudimg-settings.conf with one that contains PasswordAuthentication yes! \n"
 sleep 2
 rm /etc/ssh/sshd_config.d/60-cloudimg-settings.conf
 echo "PasswordAuthentication yes" > /etc/ssh/sshd_config.d/60-cloudimg-settings.conf
 
-printf $red "\n INFO: Rebooting now! \n"
+printf "\n $b_red INFO: $red Rebooting now! \n"
 sleep 2
 # reboot now
