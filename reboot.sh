be64e70fac0149672c07a9c56de44d26147b839c
Wesley Oliver <50159898+sharpsounds@users.noreply.github.com>
2024-09-19 00:09:35 +1000

    new code test

--- reboot.sh
+++ reboot.sh
@@ -3,9 +3,12 @@
 red='\e[31;40m'
 br_red='\e[91;40m'
 
-secs=$((5))
-while [ $secs -gt 0 ]; do
-   echo -ne "$secs\033[0K\r"
-   sleep 1
-   : $((secs--))
-done
+sleep-with-countdown() {
+  secs=$1
+  while [ $secs -gt 0 ]; do
+    printf "\rsleep: $secs\033[0K"
+    sleep 1
+    : $((secs--))
+  done
+  printf "count \n"
+}
