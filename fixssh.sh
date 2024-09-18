63c2f7e39fb770faace48101765dc8cd71508c7c
Wesley Oliver <50159898+sharpsounds@users.noreply.github.com>
2024-09-18 21:41:22 +1000

    attempt to fix linebreaks now not working

--- fixssh.sh
+++ fixssh.sh
@@ -3,7 +3,7 @@ red='\e[1;31m%s\e[0m\n'
 green='\e[1;32m%s\e[0m\n'
 yellow='\e[1;33m%s\e[0m\n'
 
-printf $green "\n INFO: Checking if script is runnning as ROOT \n"
+printf "$green" "\n INFO: Checking if script is runnning as ROOT \n"
 if [ `id -u` != '0' ]; then
     printf $red "Root privileges are required for this script."
 else
