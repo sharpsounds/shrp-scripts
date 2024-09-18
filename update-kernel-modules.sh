2a76df7c67044d56bce8669eab10af375d81e6e6
Wesley Oliver <50159898+sharpsounds@users.noreply.github.com>
2024-09-18 23:07:27 +1000

    fixed another noformat miss

--- update-kernel-modules.sh
+++ update-kernel-modules.sh
@@ -20,9 +20,9 @@ else
     printf "$br_green RESULT:$green You are ROOT.\n"
 fi
 
-printf "\n$br_yellow INFO:$yellow Now installing linux-modules-extra based on the output of $codeblock uname -r$noformat \n"
+printf "\n$br_yellow INFO:$yellow Now installing linux-modules-extra based on the output of $codeblock uname -r $noformat\n"
 kernver="$(uname -r)"
-printf "\n$yellow Current kernel version is $codeblock $kernver \n"
+printf "\n$yellow Current kernel version is $codeblock $kernver $noformat\n"
 sleep 1
 nala install -y linux-modules-extra-$kernver
 printf "\n$br_green Module install/upgrade is complete.$br_red You should now reboot! \n"
