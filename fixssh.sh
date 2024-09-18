487a3975114e092a6b13fdc2919e36d0c84251fc
Wesley Oliver <50159898+sharpsounds@users.noreply.github.com>
2024-09-18 20:58:01 +1000

    added Sudo and Sleep

--- fixssh.sh
+++ fixssh.sh
@@ -2,14 +2,18 @@
 
 clear
 printf "\n Appending PasswordAuthentication yes to /etc/ssh/ssh_config! \n"
-echo "PasswordAuthentication yes" >> /etc/ssh/ssh_config
+bash sleep 2
+sudo echo "PasswordAuthentication yes" >> /etc/ssh/ssh_config
 
 printf "\n Appending PasswordAuthentication yes to /etc/ssh/sshd_config! \n"
-echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
+bash sleep 2
+sudo echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
 
 printf "\n Replacing /etc/ssh/sshd_config.d/60-cloudimg-settings.conf with one that contains PasswordAuthentication yes! \n"
+bash sleep 2
 rm /etc/ssh/sshd_config.d/60-cloudimg-settings.conf
-echo "PasswordAuthentication yes" > /etc/ssh/sshd_config.d/60-cloudimg-settings.conf
+sudo echo "PasswordAuthentication yes" > /etc/ssh/sshd_config.d/60-cloudimg-settings.conf
 
 printf "\n Rebooting! \n"
+bash sleep 2
 sudo reboot now
