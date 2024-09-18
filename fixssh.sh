7c5338e4fbf9a4aabd4cd740f4a80d5b84ec1c85
Wesley Oliver <50159898+sharpsounds@users.noreply.github.com>
2024-09-18 22:32:28 +1000

    cleaned up colorcodes removing unused ones. added highlighting for paths and a reboot timer

--- fixssh.sh
+++ fixssh.sh
@@ -1,37 +1,14 @@
 #!/bin/bash
 
 #colorcodes
-noformat='\e[0;40m'
-black='\e[30;40m'
 red='\e[31;40m'
 green='\e[32;40m'
 yellow='\e[33;40m'
-blue='\e[34;40m'
-magenta='\e[35;40m'
-cyan='\e[36;40m'
-grey='\e[90;40m'
 br_red='\e[91;40m'
 br_green='\e[92;40m'
 br_yellow='\e[93;40m'
 br_blue='\e[94;40m'
-br_magenta='\e[95;40m'
-br_cyan='\e[96;40m'
-bg_off='\e[40m'
-bg_red='\e[41m'
-bg_green='\e[42m'
-bg_yellow='\e[43m'
-bg_blue='\e[44m'
-bg_magenta='\e[45m'
-bg_cyan='\e[46m'
-bg_grey='\e[100m'
-bg_br_red='\e[101m'
-bg_br_green='\e[102m'
-bg_br_yellow='\e[103m'
-bg_br_blue='\e[104m'
-bg_br_magenta='\e[105m'
-bg_br_cyan='\e[106m'
-
-code='\e[30;100m'
+codeblock='\e[30;100m'
 
 clear
 
@@ -45,23 +22,33 @@ fi
 
 cd /
 
-printf "\n$br_yellow INFO:$yellow Appending $code PasswordAuthentication yes $yellow to /etc/ssh/ssh_config!"
-sleep 2
+printf "\n$br_yellow INFO:$yellow Appending $codeblock PasswordAuthentication yes $yellow to $br_blue /etc/ssh/ssh_config"
+sleep 1
 echo "PasswordAuthentication yes" >> /etc/ssh/ssh_config
 printf "$br_green ...done!\n"
 
 
-printf "\n$br_yellow INFO:$yellow Appending $code PasswordAuthentication yes $yellow to /etc/ssh/sshd_config!"
-sleep 2
+printf "\n$br_yellow INFO:$yellow Appending $codeblock PasswordAuthentication yes $yellow to $br_blue /etc/ssh/sshd_config"
+sleep 1
 echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
 printf "$br_green ...done!\n"
 
-printf "\n$br_yellow INFO:$yellow Replacing /etc/ssh/sshd_config.d/60-cloudimg-settings.conf with one that contains $code PasswordAuthentication yes $yellow !"
-sleep 2
+printf "\n$br_yellow INFO:$yellow Replacing $br_blue /etc/ssh/sshd_config.d/60-cloudimg-settings.conf with one that contains $codeblock PasswordAuthentication yes"
+sleep 1
 rm /etc/ssh/sshd_config.d/60-cloudimg-settings.conf
 echo "PasswordAuthentication yes" > /etc/ssh/sshd_config.d/60-cloudimg-settings.conf
 printf "$br_green ...done!\n"
 
-printf "\n$br_red WARNING:$red Rebooting now! \n"
-sleep 2
+printf "\n$br_red WARNING:$red Rebooting in 5 seconds!\n"
+sleep 1
+printf "5"
+sleep 1
+printf "4"
+sleep 1
+printf "3"
+sleep 1
+printf "2"
+sleep 1
+printf "1"
+sleep 1
 # reboot now
