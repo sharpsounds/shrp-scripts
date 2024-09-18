e4aa2ce7a5769151bab7758fe956515e7b8c76d5
Wesley Oliver <50159898+sharpsounds@users.noreply.github.com>
2024-09-18 21:21:59 +1000

    added sleep 2

--- ubuntu24.04-cloudinit-postinstall.sh
+++ ubuntu24.04-cloudinit-postinstall.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
 
 clear
-printf "\n hello world. \n"
 printf "\n Installing Nala because I like it! \n"
+sleep 2
 apt install -y nala
 
 printf "\n Configuring prerequisites for Docker! \n"
+sleep 2
 mkdir -m 0755 -p /etc/apt/keyrings
+sleep 2
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev\null
 
 printf "\n Updating system! \n"
+sleep 2
 nala update
 nala upgrade -y
 
 printf "\n Installing some packages like!"
+sleep 2
 nala install -y qemu-guest-agent ncdu btop docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 printf "\n Installing lazydocker! \n"
+sleep 2
 curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
 mv ~/.local/bin/lazydocker /bin/lazydocker
 
 printf "\n Installing extra kernel modules! \n"
+sleep 2
 curl https://raw.githubusercontent.com/sharpsounds/shrp-scripts/main/update-kernel-modules.sh | bash
 
 # reboot now
--- update-kernel-modules.sh
+++ update-kernel-modules.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # This is so the ZigBee USB kernel drivers will be available to the system to be used in the zigbee2mqtt container.
 printf "\n Now installing linux-modules-extra based on the output of uname -r! \n"
+sleep 2
 kernver="$(uname -r)"
 printf "\n Current kernel version is $kernver! \n"
+sleep 2
 nala install linux-modules-extra-$kernver
 printf "\n Module install/upgrade is complete. You should now reboot! \n"
