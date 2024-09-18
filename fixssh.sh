3de5f40dd6a0bbd6cc4b290857d30a9a31a46345
Wesley Oliver <50159898+sharpsounds@users.noreply.github.com>
2024-09-18 21:27:52 +1000

    Added color for clarity in the terminal

--- fixssh.sh
+++ fixssh.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
+red='\e[1;31m%s\e[0m\n'                                                                                                                                                      
+green='\e[1;32m%s\e[0m\n'
+yellow='\e[1;33m%s\e[0m\n'
+
 cd /
 clear
-printf "\n Appending PasswordAuthentication yes to /etc/ssh/ssh_config! \n"
+printf $green "\n INFO: Appending PasswordAuthentication yes to /etc/ssh/ssh_config! \n"
 sleep 2
 echo "PasswordAuthentication yes" >> /etc/ssh/ssh_config
 
-printf "\n Appending PasswordAuthentication yes to /etc/ssh/sshd_config! \n"
+printf $green "\n INFO: Appending PasswordAuthentication yes to /etc/ssh/sshd_config! \n"
 sleep 2
 echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
 
-printf "\n Replacing /etc/ssh/sshd_config.d/60-cloudimg-settings.conf with one that contains PasswordAuthentication yes! \n"
+printf $green "\n INFO: Replacing /etc/ssh/sshd_config.d/60-cloudimg-settings.conf with one that contains PasswordAuthentication yes! \n"
 sleep 2
 rm /etc/ssh/sshd_config.d/60-cloudimg-settings.conf
 echo "PasswordAuthentication yes" > /etc/ssh/sshd_config.d/60-cloudimg-settings.conf
 
-printf "\n Rebooting! \n"
+printf $red "\n INFO: Rebooting now! \n"
 sleep 2
 # reboot now
