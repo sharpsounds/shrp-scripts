90d30258cbf6ade9cda473dd85de44b9b407e9bd
Wesley Oliver <50159898+sharpsounds@users.noreply.github.com>
2024-09-18 20:09:32 +1000

    added verbosity to kernel modules update script

--- update-kernel-modules.sh
+++ update-kernel-modules.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-echo now installing linux-modules-extra based on the output of uname -r!
+# this is so that the ZigBee USB kernel drivers will be available to the system to be used in the zigbee2mqtt container
+printf Now installing linux-modules-extra based on the output of uname -r!
 kernver="$(uname -r)"
+printf Current kernel version is $kernver!
 sudo nala install linux-modules-extra-$kernver
+printf Module install/upgrade is complete. You should now reboot!
