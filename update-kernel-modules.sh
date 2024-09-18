26007b005e5db3f4166b4cbeace962d80650b91f
Wesley Oliver <50159898+sharpsounds@users.noreply.github.com>
2024-09-18 20:11:22 +1000

    wording

--- update-kernel-modules.sh
+++ update-kernel-modules.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# this is so that the ZigBee USB kernel drivers will be available to the system to be used in the zigbee2mqtt container
+# This is so the ZigBee USB kernel drivers will be available to the system to be used in the zigbee2mqtt container.
 printf Now installing linux-modules-extra based on the output of uname -r!
 kernver="$(uname -r)"
 printf Current kernel version is $kernver!
