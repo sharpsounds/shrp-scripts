c4299bc3ea281cc0abc20d49c4f6d2abd9259f80
Wesley Oliver <50159898+sharpsounds@users.noreply.github.com>
2024-09-18 21:30:50 +1000

    root check

--- fixssh.sh
+++ fixssh.sh
@@ -3,6 +3,14 @@ red='\e[1;31m%s\e[0m\n'
 green='\e[1;32m%s\e[0m\n'
 yellow='\e[1;33m%s\e[0m\n'
 
+printf $green "\n INFO: Checking if script is runnning as ROOT \n"
+if [ `id -u` != '0' ]; then
+    printf $red "Root privileges are required for this script."
+else
+    echo $green "You are root."
+    exit
+fi
+
 cd /
 clear
 printf $green "\n INFO: Appending PasswordAuthentication yes to /etc/ssh/ssh_config! \n"
