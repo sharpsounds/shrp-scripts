af1fae16244d516c14fe91458ecdf4bbb8798127
Wesley Oliver <50159898+sharpsounds@users.noreply.github.com>
2024-09-18 21:46:04 +1000

    move clear to top of script

--- fixssh.sh
+++ fixssh.sh
@@ -2,6 +2,7 @@
 red='\e[1;31m%s\e[0m\n'                                                                                                                                                      
 green='\e[1;32m%s\e[0m\n'
 yellow='\e[1;33m%s\e[0m\n'
+clear
 
 printf "$green" "\n INFO: Checking if script is runnning as ROOT \n"
 if [ `id -u` != '0' ]; then
@@ -11,7 +12,6 @@ else
 fi
 
 cd /
-clear
 printf $green "\n INFO: Appending PasswordAuthentication yes to /etc/ssh/ssh_config! \n"
 sleep 2
 echo "PasswordAuthentication yes" >> /etc/ssh/ssh_config
